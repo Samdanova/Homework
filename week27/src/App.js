14d75dc0cd4966c0f1abc9e2340cc3ee00d071ac
Samdanova <samdanova123@gmail.com>
2022-04-26 19:12:05 +0300

    structure

---
+++ week27/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+// import { DatePicker } from 'antd';
+import './App.css';
+import Card from './components/card';
+
+function App() {
+  return (
+   <React.Fragment>
+  
+   </React.Fragment>
+  );
+}
+
+export default App;
