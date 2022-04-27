011c023feadd4bae4407b018166c9f94f1a6dcee
Samdanova <samdanova123@gmail.com>
2022-04-27 17:01:59 +0300

    задание выполнено

--- week27/src/App.js
+++ week27/src/App.js
@@ -1,15 +1,59 @@
 import React from 'react';
 import 'antd/dist/antd.css';
-// import { DatePicker } from 'antd';
+import { Row } from 'antd';
 import './App.css';
-import Card from './components/card';
+import CardTariffs from './components/Card';
+
+const tariffs = [
+    {
+        name: "Безлимитный 300",
+        price: "300 руб/мес",
+        speed: "до 10 Мбит/сек",
+        details: "Объем включенного трафика не ограничен",
+        isSelected: false,
+        colorBG:"blue",
+    }, {
+        name: "Безлимитный 450",
+        price: "450 руб/мес",
+        speed: "до 50 Мбит/сек",
+        details: "Объем включенного трафика не ограничен",
+        isSelected: false,
+        colorBG:"green",
+    }, {
+        name: "Безлимитный 550",
+        price: "550 руб/мес",
+        speed: "до 100 Мбит/сек",
+        details: "Объем включенного трафика не ограничен",
+        isSelected: true,
+        colorBG:"red"
+    }, {
+        name: "Безлимитный 1000",
+        price: "1000 руб/мес",
+        speed: "до 200 Мбит/сек",
+        details: "Объем включенного трафика не ограничен",
+        isSelected: false,
+        colorBG:"black"
+    }
+]
 
 function App() {
-  return (
-   <React.Fragment>
-  
-   </React.Fragment>
-  );
+    return (
+        <div className="site-card-border-less-wrapper">
+            <Row>
+                {
+                    tariffs.map(
+                        (tariff) => <CardTariffs
+                            name={tariff.name}
+                            price={tariff.price}
+                            speed={tariff.speed}
+                            details={tariff.details}
+                            isSelected={tariff.isSelected}
+                            colorBG={tariff.colorBG}></CardTariffs>
+                    )
+                }
+            </Row>
+        </div>
+    );
 }
 
 export default App;
--- week27/src/components/card.jsx
+++ week27/src/components/card.jsx
@@ -1,5 +1,25 @@
-function Card(props) {
+import React from 'react';
+
+import { Card, Col } from 'antd';
+import './Card.scss';
+
+const { Meta } = Card;
+function CardTariffs(props) {
     return (
+        <Col span={6}>
+            <Card hoverable bodyStyle={{ border: props.isSelected && "5px solid rgb(180, 216, 245)" }} cover={<div className={props.colorBG}>
+                <h1 className='cardTitle'>{props.name}</h1>
+                <hr />
+                <div className='cardPrice'>{props.price}</div>
+            </div>}
+                className='card-body'
+            >
+                <div className='cardSpeed'>{props.speed}</div>
+                <div className='cardDetails'>{props.details}</div>
+            </Card>
+        </Col>
 
     )
 }
+
+export default CardTariffs;
