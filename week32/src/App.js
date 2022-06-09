882e230df7be87afc502f0a8c2e9b82775be51b1
Samdanova <samdanova123@gmail.com>
2022-06-09 13:38:41 +0300

    32

---
+++ week32/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Chat from './assets/components/chat';
+
+function App() {
+  return (
+    <div className="App">
+        <Chat></Chat>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ week32/src/assets/components/chat.jsx
@@ -0,0 +1,38 @@
+
+import React, { useState } from 'react';
+import "./chat.css"
+export default function Chat() {
+
+    const [message, setState] = useState(''); //состояние для инпута
+    const [messages, setMessages] = useState([]); //состояние сообщений чата
+    console.log(messages)
+    const result = messages.map((item, index) => {
+        return <div className={index === 0 ? 'message last' : 'message'} key={index}>{item}</div>
+    })
+
+    const handleChange = (event) => {
+        const target = event.target.value.toLowerCase()
+        let changeTarget;
+        if ((target.includes('viagra')) || (target.includes('xxx'))) {
+            changeTarget = target.replace(/viagra|xxx/g, "***");
+        } else {
+            changeTarget = target;
+        }
+        setState(changeTarget)
+    }
+    const Send = () => {
+        setMessages([message, ...messages]);
+        setState("");
+    }
+    return (
+        <div className="chat">
+            <div className='chat-messages'>
+                {result}
+            </div>
+            <div className="chat-input">
+                <input type="text" className="chat-form__input" placeholder="Введите сообщение" value={message} onChange={handleChange} />
+                <button className="submit" onClick={Send}>Отправить</button>
+            </div>
+        </div >
+    )
+}
